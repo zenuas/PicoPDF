b694be229f817bfdd120ff7b0c38ac411ae5f1fe
zenuas@gmail.com <zenuas@gmail.com>
2023-09-23 13:18:13 +0900

    必要なくなったデータクラス削除

--- src/Section/TopBottom.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PicoPDF.Section;
-
-public class TopBottom
-{
-    public int Top { get; set; } = 0;
-    public required int Bottom { get; set; }
-
-    public int Height { get => Bottom - Top; }
-}
