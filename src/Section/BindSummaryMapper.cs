0df3f87dc2fbae3d1b33501ba937747136132104
zenuas@gmail.com <zenuas@gmail.com>
2023-09-23 13:16:54 +0900

    余計なusing削除

--- src/Section/BindSummaryMapper.cs
+++ src/Section/BindSummaryMapper.cs
@@ -4,7 +4,6 @@ using PicoPDF.Section.Element;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace PicoPDF.Section;
 
