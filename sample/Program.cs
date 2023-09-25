b4cb77375fb5a343b2ad2e0b77cbfc6e18f4cd80
zenuas@gmail.com <zenuas@gmail.com>
2023-09-25 20:33:56 +0900

    サンプルの記述をREADMEと同じフォーマットに変更

--- sample/Program.cs
+++ sample/Program.cs
@@ -1,20 +1,14 @@
-﻿using PicoPDF.Binder;
-using PicoPDF.Model;
-using PicoPDF.Pdf;
-using PicoPDF.Sample;
-using System.Text;
-
-Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
-var doc = new Document();
-doc.FontRegister.RegistDirectory(Environment.ExpandEnvironmentVariables(@"%SystemRoot%\Fonts"));
+﻿using PicoPDF.Sample;
 
 var csv = File.ReadAllLines("test-case/test.csv")
     .Skip(1)
     .Select(x => x.Split(','))
     .Select(x => new Data(x[0], x[1], x[2], int.Parse(x[3])))
     .ToArray();
-var pagesection = JsonLoader.Load("test-case/01.json");
-var pages = SectionBinder.Bind(pagesection, csv);
-ModelMapping.Mapping(doc, pages);
+
+var doc = new PicoPDF.Pdf.Document();
+doc.FontRegister.RegistDirectory(Environment.ExpandEnvironmentVariables(@"%SystemRoot%\Fonts"));
+var pagesection = PicoPDF.Binder.JsonLoader.Load("test-case/01.json");
+var pages = PicoPDF.Binder.SectionBinder.Bind(pagesection, csv);
+PicoPDF.Model.ModelMapping.Mapping(doc, pages);
 doc.Save("@a.pdf", new() { ContentsStreamDeflate = false });
