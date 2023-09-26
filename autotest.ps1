5645b6e076763bf34db8bd11b1951cf10e5b7690
zenuas@gmail.com <zenuas@gmail.com>
2023-09-27 00:07:32 +0900

    サンプル画像が更新されると再度開きなおす

--- autotest.ps1
+++ autotest.ps1
@@ -2,15 +2,29 @@ $watcher = New-Object System.IO.FileSystemWatcher
 $watcher.Path = "test-case"
 $watcher.Filter = "*.*"
 $watcher.IncludeSubdirectories = $true
-$watcher.NotifyFilter          = [System.IO.NotifyFilters]::LastAccess
+$watcher.NotifyFilter = [System.IO.NotifyFilters]::LastAccess
+
+$watcher2 = New-Object System.IO.FileSystemWatcher
+$watcher2.Path = "."
+$watcher2.Filter = "@b.bmp"
+$watcher2.IncludeSubdirectories = $true
+$watcher2.NotifyFilter = [System.IO.NotifyFilters]::LastAccess
+
 
 $action =
 {
 	Start-Process -FilePath "sample/bin/Debug/net7.0/PicoPDF.Sample.exe" -WorkingDirectory "." -NoNewWindow -Wait
-	Write-Host "$(Get-Date) done"
+	Write-Host "$(Get-Date) PicoPDF.Sample.exe done"
 }
 
-Register-ObjectEvent $watcher "Changed" -Action $action
+$action2 =
+{
+	Start-Process -FilePath "cmd" -ArgumentList "/c", "start", "@b.bmp" -WorkingDirectory "." -NoNewWindow
+	Write-Host "$(Get-Date) start @b.bmp done"
+}
+
+Register-ObjectEvent $watcher  "Changed" -Action $action
+Register-ObjectEvent $watcher2 "Changed" -Action $action2
 Invoke-Command $action
 
 while ($true)
