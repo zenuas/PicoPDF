09db17ee6293635b26a04dcafa903dc4fb96f638
zenuas@gmail.com <zenuas@gmail.com>
2024-05-06 23:08:05 +0900

    xUnitの不適切な警告抑止

--- test/JpegFileTest.cs
+++ test/JpegFileTest.cs
@@ -11,8 +11,8 @@ public class JpegFileTest
     {
         var image = ImageLoader.FromFile("../../../../test-case/300x150.jpg", ImageTypes.Jpeg);
         var jpeg = Assert.IsType<JpegFile>(image);
-        Assert.Equal(300, jpeg.Width);
-        Assert.Equal(150, jpeg.Height);
-        Assert.Equal(8, jpeg.Precision);
+        Assert.Equal(jpeg.Width, 300);
+        Assert.Equal(jpeg.Height, 150);
+        Assert.Equal(jpeg.Precision, 8);
     }
 }
