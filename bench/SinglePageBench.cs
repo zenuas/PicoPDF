fde226c8ebf89b3625139edcc07b62abe5506e32
zenuas@gmail.com <zenuas@gmail.com>
2024-05-06 23:06:19 +0900

    文字列リテラルに変更

--- bench/SinglePageBench.cs
+++ bench/SinglePageBench.cs
@@ -15,35 +15,35 @@ namespace PicoPDF.Benchmark;
 
 public class SinglePageBench
 {
-    public static PageSection PageSection { get; } = JsonLoader.LoadJsonString(@"
+    public static PageSection PageSection { get; } = JsonLoader.LoadJsonString("""
 {
-	""Size"": ""A4"",
-	""Orientation"": ""Vertical"",
-	""DefaultFont"": ""Meiryo-Bold"",
-	""Padding"": [15, 10, 15],
+	"Size": "A4",
+	"Orientation": "Vertical",
+	"DefaultFont": "Meiryo-Bold",
+	"Padding": [15, 10, 15],
 	
-	""Header"": ""PageHeader"",
-	""Detail"": {
-			""Detail"": ""Detail"",
+	"Header": "PageHeader",
+	"Detail": {
+			"Detail": "Detail",
 		},
-	""Footer"": ""PageFooter"",
+	"Footer": "PageFooter",
 	
-	""Sections"": [
-		{""Type"": ""HeaderSection"", ""Name"": ""PageHeader"", ""Height"": 50, ""ViewMode"": ""PageFirst"", ""Elements"": [
-			{""Type"": ""TextElement"", ""Text"": ""PageHeader"", ""Size"": 30, ""X"": 10, ""Y"": 0},
+	"Sections": [
+		{"Type": "HeaderSection", "Name": "PageHeader", "Height": 50, "ViewMode": "PageFirst", "Elements": [
+			{"Type": "TextElement", "Text": "PageHeader", "Size": 30, "X": 10, "Y": 0},
 		]},
-		{""Type"": ""DetailSection"", ""Name"": ""Detail"", ""Height"": 20, ""Elements"": [
-			{""Type"": ""BindElement"", ""Bind"": ""Foo"",  ""Size"": 10, ""X"": 10, ""Y"": 0},
+		{"Type": "DetailSection", "Name": "Detail", "Height": 20, "Elements": [
+			{"Type": "BindElement", "Bind": "Foo",  "Size": 10, "X": 10, "Y": 0},
 		]},
-		{""Type"": ""FooterSection"", ""Name"": ""PageFooter"", ""Height"": 30, ""Elements"": [
-			{""Type"": ""TextElement"", ""Text"": ""PageFooter"", ""Size"": 20, ""X"": 10,  ""Y"": 0, ""Font"": ""HGMinchoB""},
-			{""Type"": ""SummaryElement"",               ""Size"": 10, ""X"": 300, ""Y"": 0, ""Format"": ""#,0"", ""SummaryType"": ""PageCount"", ""Alignment"": ""End"",   ""Width"": 50},
-			{""Type"": ""TextElement"", ""Text"": ""/"", ""Size"": 10, ""X"": 352, ""Y"": 0},
-			{""Type"": ""SummaryElement"",               ""Size"": 10, ""X"": 360, ""Y"": 0, ""Format"": ""#,0"", ""SummaryType"": ""PageCount"", ""Alignment"": ""Start"", ""Width"": 50, ""SummaryMethod"": ""All""},
+		{"Type": "FooterSection", "Name": "PageFooter", "Height": 30, "Elements": [
+			{"Type": "TextElement", "Text": "PageFooter", "Size": 20, "X": 10,  "Y": 0, "Font": "HGMinchoB"},
+			{"Type": "SummaryElement",               "Size": 10, "X": 300, "Y": 0, "Format": "#,0", "SummaryType": "PageCount", "Alignment": "End",   "Width": 50},
+			{"Type": "TextElement", "Text": "/", "Size": 10, "X": 352, "Y": 0},
+			{"Type": "SummaryElement",               "Size": 10, "X": 360, "Y": 0, "Format": "#,0", "SummaryType": "PageCount", "Alignment": "Start", "Width": 50, "SummaryMethod": "All"},
 		]},
 	],
 }
-");
+""");
 
     public static FontRegister FontRegister { get; } = new FontRegister().Return(x => x.RegistDirectory(Environment.ExpandEnvironmentVariables(@"%SystemRoot%\Fonts")));
 
