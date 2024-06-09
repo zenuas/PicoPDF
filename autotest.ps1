59cb39fc2941ca74bcf199c7af7f33f60ba36e59
zenuas@gmail.com <zenuas@gmail.com>
2024-06-09 11:19:48 +0900

    テストケースの自動更新を修正

--- autotest.ps1
+++ autotest.ps1
@@ -4,28 +4,24 @@ $watcher.Filter = "*.*"
 $watcher.IncludeSubdirectories = $true
 $watcher.NotifyFilter = [System.IO.NotifyFilters]::LastWrite
 
-$watcher2 = New-Object System.IO.FileSystemWatcher
-$watcher2.Path = "."
-$watcher2.Filter = "@b.bmp"
-$watcher2.IncludeSubdirectories = $true
-$watcher2.NotifyFilter = [System.IO.NotifyFilters]::LastWrite
 
+$testall =
+{
+	Start-Process -FilePath "dotnet" -ArgumentList "run", "--project", "test-all\PicoPDF.TestAll.csproj" -WorkingDirectory "." -NoNewWindow -Wait
+	Write-Host "$(Get-Date) dotnet test-all done"
+}
 
 $action =
 {
-	Start-Process -FilePath "sample/bin/Debug/net7.0/PicoPDF.Sample.exe" -WorkingDirectory "." -NoNewWindow -Wait
-	Write-Host "$(Get-Date) PicoPDF.Sample.exe done"
+	$exten = [System.IO.Path]::GetExtension($Event.SourceEventArgs.FullPath)
+	if ($exten -ne ".pdf")
+	{
+		Invoke-Command $testall
+	}
 }
 
-$action2 =
-{
-	Start-Process -FilePath "cmd" -ArgumentList "/c", "start", "@b.bmp" -WorkingDirectory "." -NoNewWindow
-	Write-Host "$(Get-Date) start @b.bmp done"
-}
-
-Register-ObjectEvent $watcher  "Changed" -Action $action
-Register-ObjectEvent $watcher2 "Changed" -Action $action2
-Invoke-Command $action
+Register-ObjectEvent $watcher "Changed" -Action $action
+Invoke-Command $testall
 
 while ($true)
 {
