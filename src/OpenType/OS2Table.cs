67b5e5dafb150ff9fac5512f8c5da6cb3a012deb
zenuas@gmail.com <zenuas@gmail.com>
2024-06-22 12:55:49 +0900

    コンストラクタを使用しないように改善

--- src/OpenType/OS2Table.cs
+++ src/OpenType/OS2Table.cs
@@ -7,101 +7,93 @@ namespace PicoPDF.OpenType;
 
 public class OS2Table
 {
-    public readonly ushort Version;
-    public readonly short XAvgCharWidth;
-    public readonly ushort UsWeightClass;
-    public readonly ushort UsWidthClass;
-    public readonly ushort FsType;
-    public readonly short YSubscriptXSize;
-    public readonly short YSubscriptYSize;
-    public readonly short YSubscriptXOffset;
-    public readonly short YSubscriptYOffset;
-    public readonly short YSuperscriptXSize;
-    public readonly short YSuperscriptYSize;
-    public readonly short YSuperscriptXOffset;
-    public readonly short YSuperscriptYOffset;
-    public readonly short YStrikeoutSize;
-    public readonly short YStrikeoutPosition;
-    public readonly short SFamilyClass;
-    public readonly byte[] Panose;
-    public readonly uint UlUnicodeRange1;
-    public readonly uint UlUnicodeRange2;
-    public readonly uint UlUnicodeRange3;
-    public readonly uint UlUnicodeRange4;
-    public readonly string AchVendID;
-    public readonly ushort FsSelection;
-    public readonly ushort UsFirstCharIndex;
-    public readonly ushort UsLastCharIndex;
-    public readonly short STypoAscender;
-    public readonly short STypoDescender;
-    public readonly short STypoLineGap;
-    public readonly ushort UsWinAscent;
-    public readonly ushort UsWinDescent;
+    public required ushort Version { get; init; }
+    public required short XAvgCharWidth { get; init; }
+    public required ushort UsWeightClass { get; init; }
+    public required ushort UsWidthClass { get; init; }
+    public required ushort FsType { get; init; }
+    public required short YSubscriptXSize { get; init; }
+    public required short YSubscriptYSize { get; init; }
+    public required short YSubscriptXOffset { get; init; }
+    public required short YSubscriptYOffset { get; init; }
+    public required short YSuperscriptXSize { get; init; }
+    public required short YSuperscriptYSize { get; init; }
+    public required short YSuperscriptXOffset { get; init; }
+    public required short YSuperscriptYOffset { get; init; }
+    public required short YStrikeoutSize { get; init; }
+    public required short YStrikeoutPosition { get; init; }
+    public required short SFamilyClass { get; init; }
+    public required byte[] Panose { get; init; }
+    public required uint UlUnicodeRange1 { get; init; }
+    public required uint UlUnicodeRange2 { get; init; }
+    public required uint UlUnicodeRange3 { get; init; }
+    public required uint UlUnicodeRange4 { get; init; }
+    public required string AchVendID { get; init; }
+    public required ushort FsSelection { get; init; }
+    public required ushort UsFirstCharIndex { get; init; }
+    public required ushort UsLastCharIndex { get; init; }
+    public required short STypoAscender { get; init; }
+    public required short STypoDescender { get; init; }
+    public required short STypoLineGap { get; init; }
+    public required ushort UsWinAscent { get; init; }
+    public required ushort UsWinDescent { get; init; }
 
-    public readonly uint UlCodePageRange1;
-    public readonly uint UlCodePageRange2;
+    public uint UlCodePageRange1 { get; init; }
+    public uint UlCodePageRange2 { get; init; }
 
-    public readonly short SxHeight;
-    public readonly short SCapHeight;
-    public readonly ushort UsDefaultChar;
-    public readonly ushort UsBreakChar;
-    public readonly ushort UsMaxContext;
+    public short SxHeight { get; init; }
+    public short SCapHeight { get; init; }
+    public ushort UsDefaultChar { get; init; }
+    public ushort UsBreakChar { get; init; }
+    public ushort UsMaxContext { get; init; }
 
-    public readonly ushort UsLowerOpticalPointSize;
-    public readonly ushort UsUpperOpticalPointSize;
+    public ushort UsLowerOpticalPointSize { get; init; }
+    public ushort UsUpperOpticalPointSize { get; init; }
 
-    public OS2Table(Stream stream)
+    public static OS2Table ReadFrom(Stream stream)
     {
-        Version = BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2));
-        XAvgCharWidth = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2));
-        UsWeightClass = BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2));
-        UsWidthClass = BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2));
-        FsType = BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2));
-        YSubscriptXSize = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2));
-        YSubscriptYSize = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2));
-        YSubscriptXOffset = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2));
-        YSubscriptYOffset = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2));
-        YSuperscriptXSize = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2));
-        YSuperscriptYSize = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2));
-        YSuperscriptXOffset = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2));
-        YSuperscriptYOffset = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2));
-        YStrikeoutSize = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2));
-        YStrikeoutPosition = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2));
-        SFamilyClass = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2));
-        Panose = stream.ReadBytes(10);
-        UlUnicodeRange1 = BinaryPrimitives.ReadUInt32BigEndian(stream.ReadBytes(4));
-        UlUnicodeRange2 = BinaryPrimitives.ReadUInt32BigEndian(stream.ReadBytes(4));
-        UlUnicodeRange3 = BinaryPrimitives.ReadUInt32BigEndian(stream.ReadBytes(4));
-        UlUnicodeRange4 = BinaryPrimitives.ReadUInt32BigEndian(stream.ReadBytes(4));
-        AchVendID = Encoding.ASCII.GetString(stream.ReadBytes(4));
-        FsSelection = BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2));
-        UsFirstCharIndex = BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2));
-        UsLastCharIndex = BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2));
-        STypoAscender = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2));
-        STypoDescender = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2));
-        STypoLineGap = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2));
-        UsWinAscent = BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2));
-        UsWinDescent = BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2));
-
-        if (Version >= 1)
+        var version = BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2));
+        return new()
         {
-            UlCodePageRange1 = BinaryPrimitives.ReadUInt32BigEndian(stream.ReadBytes(4));
-            UlCodePageRange2 = BinaryPrimitives.ReadUInt32BigEndian(stream.ReadBytes(4));
-        }
-
-        if (Version >= 2)
-        {
-            SxHeight = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2));
-            SCapHeight = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2));
-            UsDefaultChar = BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2));
-            UsBreakChar = BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2));
-            UsMaxContext = BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2));
-        }
-
-        if (Version >= 5)
-        {
-            UsLowerOpticalPointSize = BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2));
-            UsUpperOpticalPointSize = BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2));
-        }
+            Version = version,
+            XAvgCharWidth = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2)),
+            UsWeightClass = BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2)),
+            UsWidthClass = BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2)),
+            FsType = BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2)),
+            YSubscriptXSize = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2)),
+            YSubscriptYSize = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2)),
+            YSubscriptXOffset = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2)),
+            YSubscriptYOffset = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2)),
+            YSuperscriptXSize = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2)),
+            YSuperscriptYSize = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2)),
+            YSuperscriptXOffset = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2)),
+            YSuperscriptYOffset = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2)),
+            YStrikeoutSize = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2)),
+            YStrikeoutPosition = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2)),
+            SFamilyClass = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2)),
+            Panose = stream.ReadBytes(10),
+            UlUnicodeRange1 = BinaryPrimitives.ReadUInt32BigEndian(stream.ReadBytes(4)),
+            UlUnicodeRange2 = BinaryPrimitives.ReadUInt32BigEndian(stream.ReadBytes(4)),
+            UlUnicodeRange3 = BinaryPrimitives.ReadUInt32BigEndian(stream.ReadBytes(4)),
+            UlUnicodeRange4 = BinaryPrimitives.ReadUInt32BigEndian(stream.ReadBytes(4)),
+            AchVendID = Encoding.ASCII.GetString(stream.ReadBytes(4)),
+            FsSelection = BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2)),
+            UsFirstCharIndex = BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2)),
+            UsLastCharIndex = BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2)),
+            STypoAscender = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2)),
+            STypoDescender = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2)),
+            STypoLineGap = BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2)),
+            UsWinAscent = BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2)),
+            UsWinDescent = BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2)),
+            UlCodePageRange1 = version >= 1 ? BinaryPrimitives.ReadUInt32BigEndian(stream.ReadBytes(4)) : 0,
+            UlCodePageRange2 = version >= 1 ? BinaryPrimitives.ReadUInt32BigEndian(stream.ReadBytes(4)) : 0,
+            SxHeight = version >= 2 ? BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2)) : (short)0,
+            SCapHeight = version >= 2 ? BinaryPrimitives.ReadInt16BigEndian(stream.ReadBytes(2)) : (short)0,
+            UsDefaultChar = version >= 2 ? BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2)) : (ushort)0,
+            UsBreakChar = version >= 2 ? BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2)) : (ushort)0,
+            UsMaxContext = version >= 2 ? BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2)) : (ushort)0,
+            UsLowerOpticalPointSize = version >= 5 ? BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2)) : (ushort)0,
+            UsUpperOpticalPointSize = version >= 5 ? BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2)) : (ushort)0,
+        };
     }
 }
