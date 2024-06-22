1c896670337e9d45d7751fad80131a4b07e84531
zenuas@gmail.com <zenuas@gmail.com>
2024-06-22 12:14:28 +0900

    コンストラクタを使用しないように改善

--- src/OpenType/EncodingRecord.cs
+++ src/OpenType/EncodingRecord.cs
@@ -5,13 +5,20 @@ using System.Linq;
 
 namespace PicoPDF.OpenType;
 
-public class EncodingRecord(Stream stream)
+public class EncodingRecord
 {
-    public readonly ushort PlatformID = BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2));
-    public readonly ushort EncodingID = BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2));
-    public readonly uint Offset = BinaryPrimitives.ReadUInt32BigEndian(stream.ReadBytes(4));
+    public required ushort PlatformID { get; init; }
+    public required ushort EncodingID { get; init; }
+    public required uint Offset { get; init; }
 
     public PlatformEncodings PlatformEncoding { get => (PlatformEncodings)(((uint)PlatformID << 16) | EncodingID); }
+
+    public static EncodingRecord ReadFrom(Stream stream) => new()
+    {
+        PlatformID = BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2)),
+        EncodingID = BinaryPrimitives.ReadUInt16BigEndian(stream.ReadBytes(2)),
+        Offset = BinaryPrimitives.ReadUInt32BigEndian(stream.ReadBytes(4)),
+    };
 
     public static EncodingRecord[] ReadFrom(Stream stream, TableRecord rec)
     {
@@ -19,7 +26,7 @@ public class EncodingRecord(Stream stream)
         var buffer = new MemoryStream(bytes);
         var cmaptable = CMapTable.ReadFrom(buffer);
         return Enumerable.Range(0, cmaptable.NumberOfTables)
-            .Select(_ => new EncodingRecord(buffer))
+            .Select(_ => ReadFrom(buffer))
             .ToArray();
     }
 
