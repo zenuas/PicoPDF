d9754406e34af9571df28d097b6c58b756cbcc2b
zenuas@gmail.com <zenuas@gmail.com>
2024-06-22 20:28:35 +0900

    2ページ目以降の文字が取得もれるのを修正

--- src/Pdf/Font/Type0Font.cs
+++ src/Pdf/Font/Type0Font.cs
@@ -54,22 +54,22 @@ public class Type0Font : PdfObject, IFont
         }
     }
 
-    public Position MeasureStringBox(string s) => new()
+    public Position MeasureStringBox(string s)
     {
-        Left = 0,
-        Top = (double)(-Font.OS2.STypoAscender) / Font.FontHeader.UnitsPerEm,
-        Right = (double)Font.MeasureString(s) / 1000,
-        Bottom = (double)(-Font.OS2.STypoDescender) / Font.FontHeader.UnitsPerEm,
-    };
+        s.Each(x => Chars.Add(x));
+        return new()
+        {
+            Left = 0,
+            Top = (double)(-Font.OS2.STypoAscender) / Font.FontHeader.UnitsPerEm,
+            Right = (double)Font.MeasureString(s) / 1000,
+            Bottom = (double)(-Font.OS2.STypoDescender) / Font.FontHeader.UnitsPerEm,
+        };
+    }
 
     public IEnumerable<byte> CreateTextShowingOperator(string s)
     {
         return s
-            .Select(x =>
-            {
-                Chars.Add(x);
-                return System.Text.Encoding.ASCII.GetBytes($"{Font.CharToGIDCached(x):x4}");
-            })
+            .Select(x => System.Text.Encoding.ASCII.GetBytes($"{Font.CharToGIDCached(x):x4}"))
             .Flatten()
             .Prepend(System.Text.Encoding.ASCII.GetBytes("<")[0])
             .Concat(System.Text.Encoding.ASCII.GetBytes("> Tj"));
