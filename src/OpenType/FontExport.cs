4d22e0fe2dd5f74425c53cf866af397a6ea2e476
zenuas@gmail.com <zenuas@gmail.com>
2024-06-22 11:31:49 +0900

    FontExportのひな型追加

---
+++ src/OpenType/FontExport.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+
+namespace PicoPDF.OpenType;
+
+public static class FontExport
+{
+    public static void Export(FontInfo font, Stream stream)
+    {
+
+    }
+}
--- test-all/FontFileExport.cs
+++ test-all/FontFileExport.cs
@@ -1,4 +1,6 @@
-﻿using PicoPDF.Pdf.Font;
+﻿using PicoPDF.OpenType;
+using PicoPDF.Pdf.Font;
+using System.IO;
 
 namespace PicoPDF.TestAll;
 
@@ -7,5 +9,7 @@ public static class FontFileExport
     public static void Export(FontRegister fontreg, Option opt)
     {
         var font = fontreg.Get(FontRegister.GetFontFilePath(opt.FontFileExport));
+        using var stream = new MemoryStream();
+        FontExport.Export(font, stream);
     }
 }
