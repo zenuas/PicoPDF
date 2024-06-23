17002d71668babcd76e212cdba8c78acff24dee6
zenuas@gmail.com <zenuas@gmail.com>
2024-06-24 07:52:12 +0900

    複合アウトラインのファイルのみ追加

---
+++ src/OpenType/CompositeGlyph.cs
@@ -0,0 +1,10 @@
+﻿namespace PicoPDF.OpenType;
+
+public class CompositeGlyph : ITrueTypeGlyph
+{
+    public required short NumberOfContours { get; init; }
+    public required short XMin { get; init; }
+    public required short YMin { get; init; }
+    public required short XMax { get; init; }
+    public required short YMax { get; init; }
+}
