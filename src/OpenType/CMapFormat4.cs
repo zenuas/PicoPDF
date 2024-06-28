58d583eb3bcc327782ea3eb2b8b30b9dddaff23d
zenuas@gmail.com <zenuas@gmail.com>
2024-06-28 10:26:43 +0900

    文字とGIDからcmap Format4作成に対応 IdRangeOffsetsは使用しない

--- src/OpenType/CMapFormat4.cs
+++ src/OpenType/CMapFormat4.cs
@@ -1,6 +1,7 @@
 ﻿using Mina.Extension;
 using System;
 using System.Buffers.Binary;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -49,9 +50,10 @@ public class CMapFormat4 : ICMapFormat
         };
     }
 
-    public static CMapFormat4 CreateFormat((char Char, ushort GlyphId)[] char_gid)
+    public static CMapFormat4 CreateFormat(Dictionary<char, ushort> char_gid)
     {
-        var seg_count = char_gid.Length;
+        var start_ends = CreateStartEnds(char_gid);
+        var seg_count = start_ends.Length;
         var serach_range = Math.Pow(2, Math.Floor(Math.Log2(seg_count)));
 
         return new()
@@ -63,12 +65,28 @@ public class CMapFormat4 : ICMapFormat
             SearchRange = (ushort)serach_range,
             EntrySelector = (ushort)Math.Log2(serach_range / 2),
             RangeShift = (ushort)((seg_count * 2) - serach_range),
-            EndCode = [],
+            EndCode = start_ends.Select(x => (ushort)x.End).ToArray(),
             ReservedPad = 0,
-            StartCode = [],
-            IdDelta = [],
-            IdRangeOffsets = [],
+            StartCode = start_ends.Select(x => (ushort)x.Start).ToArray(),
+            IdDelta = start_ends.Select(x => (short)(char_gid[x.Start] - x.Start)).ToArray(),
+            IdRangeOffsets = Lists.Repeat((ushort)0).Take(seg_count).ToArray(),
             GlyphIdArray = [],
         };
+    }
+
+    public static (char Start, char End)[] CreateStartEnds(Dictionary<char, ushort> char_gid) => CreateStartEnds(char_gid.Keys.Order().ToArray(), char_gid);
+
+    public static (char Start, char End)[] CreateStartEnds(Span<char> chars, Dictionary<char, ushort> char_gid)
+    {
+        var start = chars[0];
+        var gid = char_gid[chars[0]];
+        if (chars.Length == 1) return [(start, start)];
+
+        for (var i = 1; i < chars.Length; i++)
+        {
+            if (start + i == chars[i] && gid + i == char_gid[chars[i]]) continue;
+            return [(start, chars[i - 1]), .. CreateStartEnds(chars[i..], char_gid)];
+        }
+        return [(start, chars[^1])];
     }
 }
---
+++ test/CMapFormat4Test.cs
@@ -0,0 +1,41 @@
+﻿using PicoPDF.OpenType;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace PicoPDF.Test;
+
+public class CMapFormat4Test
+{
+    [Fact]
+    public void CreateStartEndsTest()
+    {
+        Assert.Equal(CMapFormat4.CreateStartEnds(new Dictionary<char, ushort>() { ['A'] = 0 }), [('A', 'A')]);
+        Assert.Equal(CMapFormat4.CreateStartEnds(new Dictionary<char, ushort>() { ['A'] = 0, ['B'] = 1 }), [('A', 'B')]);
+        Assert.Equal(CMapFormat4.CreateStartEnds(new Dictionary<char, ushort>() { ['A'] = 0, ['B'] = 1, ['C'] = 2 }), [('A', 'C')]);
+        Assert.Equal(CMapFormat4.CreateStartEnds(new Dictionary<char, ushort>() { ['A'] = 0, ['B'] = 1, ['C'] = 2, ['D'] = 3 }), [('A', 'D')]);
+
+        Assert.Equal(CMapFormat4.CreateStartEnds(new Dictionary<char, ushort>() { ['A'] = 0, ['X'] = 1 }), [('A', 'A'), ('X', 'X')]);
+        Assert.Equal(CMapFormat4.CreateStartEnds(new Dictionary<char, ushort>() { ['A'] = 0, ['B'] = 1, ['X'] = 2 }), [('A', 'B'), ('X', 'X')]);
+        Assert.Equal(CMapFormat4.CreateStartEnds(new Dictionary<char, ushort>() { ['A'] = 0, ['B'] = 1, ['C'] = 2, ['X'] = 3 }), [('A', 'C'), ('X', 'X')]);
+        Assert.Equal(CMapFormat4.CreateStartEnds(new Dictionary<char, ushort>() { ['A'] = 0, ['B'] = 1, ['C'] = 2, ['D'] = 3, ['X'] = 4 }), [('A', 'D'), ('X', 'X')]);
+
+        Assert.Equal(CMapFormat4.CreateStartEnds(new Dictionary<char, ushort>() { ['A'] = 0, ['X'] = 1, ['Y'] = 2 }), [('A', 'A'), ('X', 'Y')]);
+        Assert.Equal(CMapFormat4.CreateStartEnds(new Dictionary<char, ushort>() { ['A'] = 0, ['B'] = 1, ['X'] = 2, ['Y'] = 3 }), [('A', 'B'), ('X', 'Y')]);
+        Assert.Equal(CMapFormat4.CreateStartEnds(new Dictionary<char, ushort>() { ['A'] = 0, ['B'] = 1, ['C'] = 2, ['X'] = 3, ['Y'] = 4 }), [('A', 'C'), ('X', 'Y')]);
+        Assert.Equal(CMapFormat4.CreateStartEnds(new Dictionary<char, ushort>() { ['A'] = 0, ['B'] = 1, ['C'] = 2, ['D'] = 3, ['X'] = 4, ['Y'] = 5 }), [('A', 'D'), ('X', 'Y')]);
+
+        Assert.Equal(CMapFormat4.CreateStartEnds(new Dictionary<char, ushort>() { ['A'] = 0, ['X'] = 1, ['Y'] = 2, ['Z'] = 3 }), [('A', 'A'), ('X', 'Z')]);
+        Assert.Equal(CMapFormat4.CreateStartEnds(new Dictionary<char, ushort>() { ['A'] = 0, ['B'] = 1, ['X'] = 2, ['Y'] = 3, ['Z'] = 4 }), [('A', 'B'), ('X', 'Z')]);
+        Assert.Equal(CMapFormat4.CreateStartEnds(new Dictionary<char, ushort>() { ['A'] = 0, ['B'] = 1, ['C'] = 2, ['X'] = 3, ['Y'] = 4, ['Z'] = 5 }), [('A', 'C'), ('X', 'Z')]);
+        Assert.Equal(CMapFormat4.CreateStartEnds(new Dictionary<char, ushort>() { ['A'] = 0, ['B'] = 1, ['C'] = 2, ['D'] = 3, ['X'] = 4, ['Y'] = 5, ['Z'] = 6 }), [('A', 'D'), ('X', 'Z')]);
+
+        Assert.Equal(CMapFormat4.CreateStartEnds(new Dictionary<char, ushort>() { ['A'] = 0, ['B'] = 2 }), [('A', 'A'), ('B', 'B')]);
+    }
+
+    [Fact]
+    public void CreateStartEndsErrorTest()
+    {
+        Assert.Throws<IndexOutOfRangeException>(() => CMapFormat4.CreateStartEnds([]));
+    }
+}
