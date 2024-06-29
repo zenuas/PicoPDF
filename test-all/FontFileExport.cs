92bef47cce951588ebc97849420ddfe81123634a
zenuas@gmail.com <zenuas@gmail.com>
2024-06-30 01:52:03 +0900

    hhea、hmtxテーブルを再構築

--- test-all/FontFileExport.cs
+++ test-all/FontFileExport.cs
@@ -1,5 +1,6 @@
 ﻿using PicoPDF.OpenType;
 using PicoPDF.Pdf.Font;
+using System;
 using System.IO;
 using System.Linq;
 
@@ -17,8 +18,8 @@ public static class FontFileExport
     {
         var chars = opt.FontExportChars.Order();
         var char_glyph = chars
-            .Select((c, i) => (Char: c, Index: (ushort)i))
-            .ToDictionary(x => x.Char, x => (x.Index, Glyph: font.Glyphs[font.MeasureChar(x.Char)]));
+            .Select((c, i) => (Char: c, Index: (ushort)i, GID: font.CharToGIDCached(c)))
+            .ToDictionary(x => x.Char, x => (x.Index, Glyph: font.Glyphs[x.GID], HorizontalMetrics: font.HorizontalMetrics.Metrics[Math.Min(x.GID, font.HorizontalHeader.NumberOfHMetrics - 1)]));
 
         var cmap4 = CMapFormat4.CreateFormat(chars.ToDictionary(x => x, x => char_glyph[x].Index));
         var cmap4_range = FontLoader.CreateCMap4Range(cmap4);
@@ -42,6 +43,34 @@ public static class FontFileExport
             MaxComponentDepth = font.MaximumProfile.MaxComponentDepth,
         };
 
+        var hhea = new HorizontalHeaderTable()
+        {
+            MajorVersion = font.HorizontalHeader.MajorVersion,
+            MinorVersion = font.HorizontalHeader.MinorVersion,
+            Ascender = font.HorizontalHeader.Ascender,
+            Descender = font.HorizontalHeader.Descender,
+            LineGap = font.HorizontalHeader.LineGap,
+            AdvanceWidthMax = font.HorizontalHeader.AdvanceWidthMax,
+            MinLeftSideBearing = font.HorizontalHeader.MinLeftSideBearing,
+            MinRightSideBearing = font.HorizontalHeader.MinRightSideBearing,
+            XMaxExtent = font.HorizontalHeader.XMaxExtent,
+            CaretSlopeRise = font.HorizontalHeader.CaretSlopeRise,
+            CaretSlopeRun = font.HorizontalHeader.CaretSlopeRun,
+            CaretOffset = font.HorizontalHeader.CaretOffset,
+            Reserved1 = font.HorizontalHeader.Reserved1,
+            Reserved2 = font.HorizontalHeader.Reserved2,
+            Reserved3 = font.HorizontalHeader.Reserved3,
+            Reserved4 = font.HorizontalHeader.Reserved4,
+            MetricDataFormat = font.HorizontalHeader.MetricDataFormat,
+            NumberOfHMetrics = (ushort)opt.FontExportChars.Length,
+        };
+
+        var hmtx = new HorizontalMetricsTable()
+        {
+            Metrics = chars.Select(x => char_glyph[x].HorizontalMetrics).ToArray(),
+            LeftSideBearing = [],
+        };
+
         var ttf = new TrueTypeFont()
         {
             PostScriptName = font.PostScriptName,
@@ -54,8 +83,8 @@ public static class FontFileExport
             MaximumProfile = maxp,
             PostScript = font.PostScript,
             OS2 = font.OS2,
-            HorizontalHeader = font.HorizontalHeader,
-            HorizontalMetrics = font.HorizontalMetrics,
+            HorizontalHeader = hhea,
+            HorizontalMetrics = hmtx,
             CMap = new() { Version = 0, NumberOfTables = 1, EncodingRecords = new() { [new() { PlatformID = (ushort)Platforms.Unicode, EncodingID = (ushort)Encodings.Unicode2_0_BMPOnly, Offset = 0 }] = cmap4 } },
             CMap4 = cmap4,
             CMap4Range = cmap4_range,
