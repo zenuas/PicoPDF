d1b29af85964594e54c4dd791451432382f45e87
zenuas@gmail.com <zenuas@gmail.com>
2024-07-15 01:45:18 +0900

    共通処理を関数化

--- src/OpenType/FontExtract.cs
+++ src/OpenType/FontExtract.cs
@@ -20,54 +20,12 @@ public static class FontExtract
             .ToDictionary(x => x.Index, x => (x.Glyph, x.HorizontalMetrics));
         var num_of_glyph = gid_glyph.Keys.Max();
 
-        var names = font.Name.NameRecords.Where(name_record => opt.OutputNames.Contains(x =>
-                (x.PlatformID is null || ((ushort)x.PlatformID == name_record.NameRecord.PlatformID)) &&
-                (x.EncodingID is null || ((ushort)x.EncodingID == name_record.NameRecord.EncodingID)) &&
-                (x.LanguageID is null || (x.LanguageID == name_record.NameRecord.LanguageID)) &&
-                (x.NameID is null || ((ushort)x.NameID == name_record.NameRecord.NameID))
-            )).ToArray();
-        var name = new NameTable() { Format = 0, Count = (ushort)names.Length, StringOffset = 0, NameRecords = names };
-
-        var maxp = new MaximumProfileTable()
-        {
-            Version = font.MaximumProfile.Version,
-            NumberOfGlyphs = (ushort)(num_of_glyph + 1),
-            MaxPoints = font.MaximumProfile.MaxPoints,
-            MaxContours = font.MaximumProfile.MaxContours,
-            MaxCompositePoints = font.MaximumProfile.MaxCompositePoints,
-            MaxCompositeContours = font.MaximumProfile.MaxCompositeContours,
-            MaxZones = font.MaximumProfile.MaxZones,
-            MaxTwilightPoints = font.MaximumProfile.MaxTwilightPoints,
-            MaxStorage = font.MaximumProfile.MaxStorage,
-            MaxFunctionDefs = font.MaximumProfile.MaxFunctionDefs,
-            MaxInstructionDefs = font.MaximumProfile.MaxInstructionDefs,
-            MaxStackElements = font.MaximumProfile.MaxStackElements,
-            MaxSizeOfInstructions = font.MaximumProfile.MaxSizeOfInstructions,
-            MaxComponentElements = font.MaximumProfile.MaxComponentElements,
-            MaxComponentDepth = font.MaximumProfile.MaxComponentDepth,
-        };
-
-        var hhea = new HorizontalHeaderTable()
-        {
-            MajorVersion = font.HorizontalHeader.MajorVersion,
-            MinorVersion = font.HorizontalHeader.MinorVersion,
-            Ascender = font.HorizontalHeader.Ascender,
-            Descender = font.HorizontalHeader.Descender,
-            LineGap = font.HorizontalHeader.LineGap,
-            AdvanceWidthMax = font.HorizontalHeader.AdvanceWidthMax,
-            MinLeftSideBearing = font.HorizontalHeader.MinLeftSideBearing,
-            MinRightSideBearing = font.HorizontalHeader.MinRightSideBearing,
-            XMaxExtent = font.HorizontalHeader.XMaxExtent,
-            CaretSlopeRise = font.HorizontalHeader.CaretSlopeRise,
-            CaretSlopeRun = font.HorizontalHeader.CaretSlopeRun,
-            CaretOffset = font.HorizontalHeader.CaretOffset,
-            Reserved1 = font.HorizontalHeader.Reserved1,
-            Reserved2 = font.HorizontalHeader.Reserved2,
-            Reserved3 = font.HorizontalHeader.Reserved3,
-            Reserved4 = font.HorizontalHeader.Reserved4,
-            MetricDataFormat = font.HorizontalHeader.MetricDataFormat,
-            NumberOfHMetrics = (ushort)(num_of_glyph + 1),
-        };
+        var name = ExtractNameTable(font.Name, opt);
+        var maxp = CopyMaximumProfileTable(font.MaximumProfile, (ushort)(num_of_glyph + 1));
+        var hhea = CopyHorizontalHeaderTable(font.HorizontalHeader, (ushort)(num_of_glyph + 1));
+        var cmap4 = CMapFormat4.CreateFormat(chars.ToDictionary(x => x, x => char_glyph[x].Index));
+        var cmap4_range = FontLoader.CreateCMap4Range(cmap4);
+        var cmap = CreateCMapTable(cmap4, opt);
 
         var hmtx = new HorizontalMetricsTable()
         {
@@ -76,18 +34,6 @@ public static class FontExtract
                 .Prepend(font.HorizontalMetrics.Metrics[0])
                 .ToArray(),
             LeftSideBearing = [],
-        };
-
-        var cmap4 = CMapFormat4.CreateFormat(chars.ToDictionary(x => x, x => char_glyph[x].Index));
-        var cmap4_range = FontLoader.CreateCMap4Range(cmap4);
-        var cmaps = opt.OutputCMap
-            .Select(x => new EncodingRecord { PlatformID = (ushort)x.PlatformID, EncodingID = (ushort)x.EncodingID, Offset = 0 })
-            .ToArray();
-        var cmap = new CMapTable()
-        {
-            Version = 0,
-            NumberOfTables = (ushort)cmaps.Length,
-            EncodingRecords = cmaps.ToDictionary(x => x, _ => (ICMapFormat)cmap4)
         };
 
         var glyf = Lists.RangeTo(1, num_of_glyph)
@@ -128,54 +74,12 @@ public static class FontExtract
             .ToDictionary(x => x.Index, x => (x.Glyph, x.HorizontalMetrics));
         var num_of_glyph = gid_glyph.Keys.Max();
 
-        var names = font.Name.NameRecords.Where(name_record => opt.OutputNames.Contains(x =>
-                (x.PlatformID is null || ((ushort)x.PlatformID == name_record.NameRecord.PlatformID)) &&
-                (x.EncodingID is null || ((ushort)x.EncodingID == name_record.NameRecord.EncodingID)) &&
-                (x.LanguageID is null || (x.LanguageID == name_record.NameRecord.LanguageID)) &&
-                (x.NameID is null || ((ushort)x.NameID == name_record.NameRecord.NameID))
-            )).ToArray();
-        var name = new NameTable() { Format = 0, Count = (ushort)names.Length, StringOffset = 0, NameRecords = names };
-
-        var maxp = new MaximumProfileTable()
-        {
-            Version = font.MaximumProfile.Version,
-            NumberOfGlyphs = (ushort)(num_of_glyph + 1),
-            MaxPoints = font.MaximumProfile.MaxPoints,
-            MaxContours = font.MaximumProfile.MaxContours,
-            MaxCompositePoints = font.MaximumProfile.MaxCompositePoints,
-            MaxCompositeContours = font.MaximumProfile.MaxCompositeContours,
-            MaxZones = font.MaximumProfile.MaxZones,
-            MaxTwilightPoints = font.MaximumProfile.MaxTwilightPoints,
-            MaxStorage = font.MaximumProfile.MaxStorage,
-            MaxFunctionDefs = font.MaximumProfile.MaxFunctionDefs,
-            MaxInstructionDefs = font.MaximumProfile.MaxInstructionDefs,
-            MaxStackElements = font.MaximumProfile.MaxStackElements,
-            MaxSizeOfInstructions = font.MaximumProfile.MaxSizeOfInstructions,
-            MaxComponentElements = font.MaximumProfile.MaxComponentElements,
-            MaxComponentDepth = font.MaximumProfile.MaxComponentDepth,
-        };
-
-        var hhea = new HorizontalHeaderTable()
-        {
-            MajorVersion = font.HorizontalHeader.MajorVersion,
-            MinorVersion = font.HorizontalHeader.MinorVersion,
-            Ascender = font.HorizontalHeader.Ascender,
-            Descender = font.HorizontalHeader.Descender,
-            LineGap = font.HorizontalHeader.LineGap,
-            AdvanceWidthMax = font.HorizontalHeader.AdvanceWidthMax,
-            MinLeftSideBearing = font.HorizontalHeader.MinLeftSideBearing,
-            MinRightSideBearing = font.HorizontalHeader.MinRightSideBearing,
-            XMaxExtent = font.HorizontalHeader.XMaxExtent,
-            CaretSlopeRise = font.HorizontalHeader.CaretSlopeRise,
-            CaretSlopeRun = font.HorizontalHeader.CaretSlopeRun,
-            CaretOffset = font.HorizontalHeader.CaretOffset,
-            Reserved1 = font.HorizontalHeader.Reserved1,
-            Reserved2 = font.HorizontalHeader.Reserved2,
-            Reserved3 = font.HorizontalHeader.Reserved3,
-            Reserved4 = font.HorizontalHeader.Reserved4,
-            MetricDataFormat = font.HorizontalHeader.MetricDataFormat,
-            NumberOfHMetrics = (ushort)(num_of_glyph + 1),
-        };
+        var name = ExtractNameTable(font.Name, opt);
+        var maxp = CopyMaximumProfileTable(font.MaximumProfile, (ushort)(num_of_glyph + 1));
+        var hhea = CopyHorizontalHeaderTable(font.HorizontalHeader, (ushort)(num_of_glyph + 1));
+        var cmap4 = CMapFormat4.CreateFormat(chars.ToDictionary(x => x, x => char_glyph[x].Index));
+        var cmap4_range = FontLoader.CreateCMap4Range(cmap4);
+        var cmap = CreateCMapTable(cmap4, opt);
 
         var hmtx = new HorizontalMetricsTable()
         {
@@ -184,18 +88,6 @@ public static class FontExtract
                 .Prepend(font.HorizontalMetrics.Metrics[0])
                 .ToArray(),
             LeftSideBearing = [],
-        };
-
-        var cmap4 = CMapFormat4.CreateFormat(chars.ToDictionary(x => x, x => char_glyph[x].Index));
-        var cmap4_range = FontLoader.CreateCMap4Range(cmap4);
-        var cmaps = opt.OutputCMap
-            .Select(x => new EncodingRecord { PlatformID = (ushort)x.PlatformID, EncodingID = (ushort)x.EncodingID, Offset = 0 })
-            .ToArray();
-        var cmap = new CMapTable()
-        {
-            Version = 0,
-            NumberOfTables = (ushort)cmaps.Length,
-            EncodingRecords = cmaps.ToDictionary(x => x, _ => (ICMapFormat)cmap4)
         };
 
         var char_strings = Lists.RangeTo(1, num_of_glyph)
@@ -235,6 +127,72 @@ public static class FontExtract
             CMap4 = cmap4,
             CMap4Range = cmap4_range,
             CompactFontFormat = cff,
+        };
+    }
+
+    public static NameTable ExtractNameTable(NameTable name, FontExtractOption opt)
+    {
+        var names = name.NameRecords.Where(name_record => opt.OutputNames.Contains(x =>
+                (x.PlatformID is null || ((ushort)x.PlatformID == name_record.NameRecord.PlatformID)) &&
+                (x.EncodingID is null || ((ushort)x.EncodingID == name_record.NameRecord.EncodingID)) &&
+                (x.LanguageID is null || (x.LanguageID == name_record.NameRecord.LanguageID)) &&
+                (x.NameID is null || ((ushort)x.NameID == name_record.NameRecord.NameID))
+            )).ToArray();
+
+        return new() { Format = 0, Count = (ushort)names.Length, StringOffset = 0, NameRecords = names };
+    }
+
+    public static MaximumProfileTable CopyMaximumProfileTable(MaximumProfileTable maxp, ushort num_of_glyph_with_notdef) => new()
+    {
+        Version = maxp.Version,
+        NumberOfGlyphs = num_of_glyph_with_notdef,
+        MaxPoints = maxp.MaxPoints,
+        MaxContours = maxp.MaxContours,
+        MaxCompositePoints = maxp.MaxCompositePoints,
+        MaxCompositeContours = maxp.MaxCompositeContours,
+        MaxZones = maxp.MaxZones,
+        MaxTwilightPoints = maxp.MaxTwilightPoints,
+        MaxStorage = maxp.MaxStorage,
+        MaxFunctionDefs = maxp.MaxFunctionDefs,
+        MaxInstructionDefs = maxp.MaxInstructionDefs,
+        MaxStackElements = maxp.MaxStackElements,
+        MaxSizeOfInstructions = maxp.MaxSizeOfInstructions,
+        MaxComponentElements = maxp.MaxComponentElements,
+        MaxComponentDepth = maxp.MaxComponentDepth,
+    };
+
+    public static HorizontalHeaderTable CopyHorizontalHeaderTable(HorizontalHeaderTable hhea, ushort num_of_glyph_with_notdef) => new()
+    {
+        MajorVersion = hhea.MajorVersion,
+        MinorVersion = hhea.MinorVersion,
+        Ascender = hhea.Ascender,
+        Descender = hhea.Descender,
+        LineGap = hhea.LineGap,
+        AdvanceWidthMax = hhea.AdvanceWidthMax,
+        MinLeftSideBearing = hhea.MinLeftSideBearing,
+        MinRightSideBearing = hhea.MinRightSideBearing,
+        XMaxExtent = hhea.XMaxExtent,
+        CaretSlopeRise = hhea.CaretSlopeRise,
+        CaretSlopeRun = hhea.CaretSlopeRun,
+        CaretOffset = hhea.CaretOffset,
+        Reserved1 = hhea.Reserved1,
+        Reserved2 = hhea.Reserved2,
+        Reserved3 = hhea.Reserved3,
+        Reserved4 = hhea.Reserved4,
+        MetricDataFormat = hhea.MetricDataFormat,
+        NumberOfHMetrics = num_of_glyph_with_notdef,
+    };
+
+    public static CMapTable CreateCMapTable(ICMapFormat cmap_format, FontExtractOption opt)
+    {
+        var cmaps = opt.OutputCMap
+            .Select(x => new EncodingRecord { PlatformID = (ushort)x.PlatformID, EncodingID = (ushort)x.EncodingID, Offset = 0 })
+            .ToArray();
+        return new()
+        {
+            Version = 0,
+            NumberOfTables = (ushort)cmaps.Length,
+            EncodingRecords = cmaps.ToDictionary(x => x, _ => cmap_format),
         };
     }
 }
