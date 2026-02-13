ef3103a129e130c3aed395947dbcea4d11d728d3
zenuas@gmail.com <zenuas@gmail.com>
2026-02-13 16:51:36 +0900

    フォントリストにOS2.FsTypeを出力

--- test-all/FontListTest.cs
+++ test-all/FontListTest.cs
@@ -10,7 +10,15 @@ public static class FontListTest
     {
         foreach (var kv in fontreg.Fonts.Where(x => opt.AllFontPreview || x.Key != FontRegister.GetFontFilePath(x.Value.Value.Path)))
         {
-            Console.WriteLine($"{kv.Key},\"{FontRegister.GetFontFilePath(kv.Value.Value.Path)}\",Offset.Version=0x{kv.Value.Value.Offset.Version:x8}");
+            try
+            {
+                var font = fontreg.LoadRequiredTables(kv.Value.Value.Path);
+                Console.WriteLine($"{kv.Key},\"{FontRegister.GetFontFilePath(font.Path)}\",Offset.Version=0x{font.Offset.Version:x8},OS2.FsType=0x{font.OS2.FsType:x4}");
+            }
+            catch
+            {
+                Console.WriteLine($"{kv.Key},\"{FontRegister.GetFontFilePath(kv.Value.Value.Path)}\",Offset.Version=0x{kv.Value.Value.Offset.Version:x8}");
+            }
         }
     }
 }
