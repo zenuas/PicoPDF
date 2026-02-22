f54b4bdb096ddeb2e256c5b87583a2fa1b693423
zenuas@gmail.com <zenuas@gmail.com>
2026-02-22 23:18:42 +0900

    バイト読み個を改善

--- src/OpenType/Tables/ColorRecord.cs
+++ src/OpenType/Tables/ColorRecord.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using Mina.Extension;
+using System.IO;
 
 namespace PicoPDF.OpenType.Tables;
 
@@ -11,10 +12,10 @@ public class ColorRecord
 
     public static ColorRecord ReadFrom(Stream stream) => new()
     {
-        Blue = (byte)stream.ReadByte(),
-        Green = (byte)stream.ReadByte(),
-        Red = (byte)stream.ReadByte(),
-        Alpha = (byte)stream.ReadByte(),
+        Blue = stream.ReadUByte(),
+        Green = stream.ReadUByte(),
+        Red = stream.ReadUByte(),
+        Alpha = stream.ReadUByte(),
     };
 
     public void WriteTo(Stream stream)
