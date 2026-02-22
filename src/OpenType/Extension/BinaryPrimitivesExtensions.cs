1a1d832e4a5f6137b1bc1e5180134d311eb2f471
zenuas@gmail.com <zenuas@gmail.com>
2026-02-23 00:39:07 +0900

    BinaryPrimitivesの拡張メンバーに変更

---
+++ src/OpenType/Extension/BinaryPrimitivesExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Buffers.Binary;
+
+namespace PicoPDF.OpenType.Extension;
+
+public static class BinaryPrimitivesExtensions
+{
+    extension(BinaryPrimitives)
+    {
+        public static int ReadInt24LittleEndian(ReadOnlySpan<byte> source) => source[0] | (source[1] << 8) | (source[2] << 16);
+        public static int ReadInt24BigEndian(ReadOnlySpan<byte> source) => (source[0] << 16) | (source[1] << 8) | source[2];
+    }
+}
