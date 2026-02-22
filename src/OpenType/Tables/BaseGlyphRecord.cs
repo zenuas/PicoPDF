611c699eab3587549a0337e89f468a0a10bd7a86
zenuas@gmail.com <zenuas@gmail.com>
2026-02-22 23:20:33 +0900

    COLRテーブル読み込み作成中

---
+++ src/OpenType/Tables/BaseGlyphRecord.cs
@@ -0,0 +1,25 @@
+﻿using Mina.Extension;
+using System.IO;
+
+namespace PicoPDF.OpenType.Tables;
+
+public class BaseGlyphRecord
+{
+    public required ushort GlyphID { get; init; }
+    public required ushort FirstLayerIndex { get; init; }
+    public required ushort NumberLayers { get; init; }
+
+    public static BaseGlyphRecord ReadFrom(Stream stream) => new()
+    {
+        GlyphID = stream.ReadUShortByBigEndian(),
+        FirstLayerIndex = stream.ReadUShortByBigEndian(),
+        NumberLayers = stream.ReadUShortByBigEndian(),
+    };
+
+    public void WriteTo(Stream stream)
+    {
+        stream.WriteUShortByBigEndian(GlyphID);
+        stream.WriteUShortByBigEndian(FirstLayerIndex);
+        stream.WriteUShortByBigEndian(NumberLayers);
+    }
+}
--- src/OpenType/Tables/ColorTable.cs
+++ src/OpenType/Tables/ColorTable.cs
@@ -1,13 +1,48 @@
-﻿using System;
+﻿using Mina.Extension;
+using System;
 using System.IO;
 
 namespace PicoPDF.OpenType.Tables;
 
 public class ColorTable : IExportable
 {
+    public required ushort Version { get; init; }
+    public required ushort NumberBaseGlyphRecords { get; init; }
+    public required uint BaseGlyphRecordsOffset { get; init; }
+    public required uint LayerRecordsOffset { get; init; }
+    public required ushort NumberLayerRecords { get; init; }
+
+
     public static ColorTable ReadFrom(Stream stream)
     {
-        return new();
+        var version = stream.ReadUShortByBigEndian();
+        var numBaseGlyphRecords = stream.ReadUShortByBigEndian();
+        var baseGlyphRecordsOffset = stream.ReadUIntByBigEndian();
+        var layerRecordsOffset = stream.ReadUIntByBigEndian();
+        var numLayerRecords = stream.ReadUShortByBigEndian();
+
+        uint baseGlyphListOffset = 0;
+        uint layerListOffset = 0;
+        uint clipListOffset = 0;
+        uint varIndexMapOffset = 0;
+        uint itemVariationStoreOffset = 0;
+        if (version >= 1)
+        {
+            baseGlyphListOffset = stream.ReadUIntByBigEndian();
+            layerListOffset = stream.ReadUIntByBigEndian();
+            clipListOffset = stream.ReadUIntByBigEndian();
+            varIndexMapOffset = stream.ReadUIntByBigEndian();
+            itemVariationStoreOffset = stream.ReadUIntByBigEndian();
+        }
+
+        return new()
+        {
+            Version = version,
+            NumberBaseGlyphRecords = numBaseGlyphRecords,
+            BaseGlyphRecordsOffset = baseGlyphRecordsOffset,
+            LayerRecordsOffset = layerRecordsOffset,
+            NumberLayerRecords = numLayerRecords,
+        };
     }
 
     public void WriteTo(Stream stream)
