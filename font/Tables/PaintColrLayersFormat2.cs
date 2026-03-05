6ce6db62406c1637c080d77c0a311d8ee86a18a8
zenuas@gmail.com <zenuas@gmail.com>
2026-03-06 08:32:06 +0900

    PaintColrLayersFormat2、PaintColrLayersFormat3を追加

---
+++ font/Tables/PaintColrLayersFormat2.cs
@@ -0,0 +1,27 @@
+﻿using Mina.Extension;
+using System.IO;
+
+namespace OpenType.Tables;
+
+public class PaintColrLayersFormat2 : IPaintColrLayersFormat
+{
+    public required byte Format { get; init; }
+    public required ushort PaletteIndex { get; init; }
+    public required ushort Alpha { get; init; }
+
+    public static PaintColrLayersFormat2 ReadFrom(Stream stream) => new()
+    {
+        Format = 2,
+        PaletteIndex = stream.ReadUShortByBigEndian(),
+        Alpha = stream.ReadUShortByBigEndian(),
+    };
+
+    public void WriteTo(Stream stream)
+    {
+        stream.WriteByte(Format);
+        stream.WriteUShortByBigEndian(PaletteIndex);
+        stream.WriteUShortByBigEndian(Alpha);
+    }
+
+    public int SizeOf() => Format.SizeOf() + PaletteIndex.SizeOf() + Alpha.SizeOf();
+}
---
+++ font/Tables/PaintColrLayersFormat3.cs
@@ -0,0 +1,30 @@
+﻿using Mina.Extension;
+using System.IO;
+
+namespace OpenType.Tables;
+
+public class PaintColrLayersFormat3 : IPaintColrLayersFormat
+{
+    public required byte Format { get; init; }
+    public required ushort PaletteIndex { get; init; }
+    public required ushort Alpha { get; init; }
+    public required uint VarIndexBase { get; init; }
+
+    public static PaintColrLayersFormat3 ReadFrom(Stream stream) => new()
+    {
+        Format = 3,
+        PaletteIndex = stream.ReadUShortByBigEndian(),
+        Alpha = stream.ReadUShortByBigEndian(),
+        VarIndexBase = stream.ReadUIntByBigEndian(),
+    };
+
+    public void WriteTo(Stream stream)
+    {
+        stream.WriteByte(Format);
+        stream.WriteUShortByBigEndian(PaletteIndex);
+        stream.WriteUShortByBigEndian(Alpha);
+        stream.WriteUIntByBigEndian(VarIndexBase);
+    }
+
+    public int SizeOf() => Format.SizeOf() + PaletteIndex.SizeOf() + Alpha.SizeOf() + VarIndexBase.SizeOf();
+}
