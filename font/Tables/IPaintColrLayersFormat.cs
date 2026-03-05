91c1d8bf9a6ae02bfc8a71b20ffd267c39486b77
zenuas@gmail.com <zenuas@gmail.com>
2026-03-06 08:24:35 +0900

    PaintColrLayersFormat1を追加

---
+++ font/Tables/IPaintColrLayersFormat.cs
@@ -0,0 +1,8 @@
+﻿namespace OpenType.Tables;
+
+public interface IPaintColrLayersFormat : IExportable
+{
+    public byte Format { get; init; }
+
+    public int SizeOf();
+}
---
+++ font/Tables/PaintColrLayersFormat1.cs
@@ -0,0 +1,27 @@
+﻿using Mina.Extension;
+using System.IO;
+
+namespace OpenType.Tables;
+
+public class PaintColrLayersFormat1 : IPaintColrLayersFormat
+{
+    public required byte Format { get; init; }
+    public required byte NumberOfLayers { get; init; }
+    public required uint FirstLayerIndex { get; init; }
+
+    public static PaintColrLayersFormat1 ReadFrom(Stream stream) => new()
+    {
+        Format = 1,
+        NumberOfLayers = stream.ReadUByte(),
+        FirstLayerIndex = stream.ReadUIntByBigEndian(),
+    };
+
+    public void WriteTo(Stream stream)
+    {
+        stream.WriteByte(Format);
+        stream.WriteByte(NumberOfLayers);
+        stream.WriteUIntByBigEndian(FirstLayerIndex);
+    }
+
+    public int SizeOf() => Format.SizeOf() + NumberOfLayers.SizeOf() + FirstLayerIndex.SizeOf();
+}
