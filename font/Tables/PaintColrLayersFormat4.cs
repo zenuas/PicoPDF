cc1c8f58b1f8b9023275f04f8c4d97f1d18bda1d
zenuas@gmail.com <zenuas@gmail.com>
2026-03-06 08:42:03 +0900

    FWORDに合わせて型修正

--- font/Tables/PaintColrLayersFormat4.cs
+++ font/Tables/PaintColrLayersFormat4.cs
@@ -8,35 +8,35 @@ public class PaintColrLayersFormat4 : IPaintColrLayersFormat
 {
     public required byte Format { get; init; }
     public required int ColorLineOffset { get; init; }
-    public required ushort X0 { get; init; }
-    public required ushort Y0 { get; init; }
-    public required ushort X1 { get; init; }
-    public required ushort Y1 { get; init; }
-    public required ushort X2 { get; init; }
-    public required ushort Y2 { get; init; }
+    public required short X0 { get; init; }
+    public required short Y0 { get; init; }
+    public required short X1 { get; init; }
+    public required short Y1 { get; init; }
+    public required short X2 { get; init; }
+    public required short Y2 { get; init; }
 
     public static PaintColrLayersFormat4 ReadFrom(Stream stream) => new()
     {
         Format = 4,
         ColorLineOffset = stream.Read3BytesByBigEndian(),
-        X0 = stream.ReadUShortByBigEndian(),
-        Y0 = stream.ReadUShortByBigEndian(),
-        X1 = stream.ReadUShortByBigEndian(),
-        Y1 = stream.ReadUShortByBigEndian(),
-        X2 = stream.ReadUShortByBigEndian(),
-        Y2 = stream.ReadUShortByBigEndian(),
+        X0 = stream.ReadShortByBigEndian(),
+        Y0 = stream.ReadShortByBigEndian(),
+        X1 = stream.ReadShortByBigEndian(),
+        Y1 = stream.ReadShortByBigEndian(),
+        X2 = stream.ReadShortByBigEndian(),
+        Y2 = stream.ReadShortByBigEndian(),
     };
 
     public void WriteTo(Stream stream)
     {
         stream.WriteByte(Format);
         stream.Write3BytesByBigEndian(ColorLineOffset);
-        stream.WriteUShortByBigEndian(X0);
-        stream.WriteUShortByBigEndian(Y0);
-        stream.WriteUShortByBigEndian(X1);
-        stream.WriteUShortByBigEndian(Y1);
-        stream.WriteUShortByBigEndian(X2);
-        stream.WriteUShortByBigEndian(Y2);
+        stream.WriteShortByBigEndian(X0);
+        stream.WriteShortByBigEndian(Y0);
+        stream.WriteShortByBigEndian(X1);
+        stream.WriteShortByBigEndian(Y1);
+        stream.WriteShortByBigEndian(X2);
+        stream.WriteShortByBigEndian(Y2);
     }
 
     public int SizeOf() => Format.SizeOf() + ColorLineOffset.SizeOf() +
