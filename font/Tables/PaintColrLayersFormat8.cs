3a2ef199d15efd03c3b7ada77539be9cfa3c6597
zenuas@gmail.com <zenuas@gmail.com>
2026-03-06 09:11:35 +0900

    PaintColrLayersFormat8、PaintColrLayersFormat9追加

---
+++ font/Tables/PaintColrLayersFormat8.cs
@@ -0,0 +1,39 @@
+﻿using Mina.Extension;
+using OpenType.Extension;
+using System.IO;
+
+namespace OpenType.Tables;
+
+public class PaintColrLayersFormat8 : IPaintColrLayersFormat
+{
+    public required byte Format { get; init; }
+    public required int ColorLineOffset { get; init; }
+    public required short CenterX { get; init; }
+    public required short CenterY { get; init; }
+    public required ushort StartAngle { get; init; }
+    public required ushort EndAngle { get; init; }
+
+    public static PaintColrLayersFormat8 ReadFrom(Stream stream) => new()
+    {
+        Format = 8,
+        ColorLineOffset = stream.Read3BytesByBigEndian(),
+        CenterX = stream.ReadShortByBigEndian(),
+        CenterY = stream.ReadShortByBigEndian(),
+        StartAngle = stream.ReadUShortByBigEndian(),
+        EndAngle = stream.ReadUShortByBigEndian(),
+    };
+
+    public void WriteTo(Stream stream)
+    {
+        stream.WriteByte(Format);
+        stream.Write3BytesByBigEndian(ColorLineOffset);
+        stream.WriteShortByBigEndian(CenterX);
+        stream.WriteShortByBigEndian(CenterY);
+        stream.WriteUShortByBigEndian(StartAngle);
+        stream.WriteUShortByBigEndian(EndAngle);
+    }
+
+    public int SizeOf() => Format.SizeOf() + /* ColorLineOffset sizeof(Offset24) */3 +
+        CenterX.SizeOf() + CenterY.SizeOf() +
+        StartAngle.SizeOf() + EndAngle.SizeOf();
+}
---
+++ font/Tables/PaintColrLayersFormat9.cs
@@ -0,0 +1,43 @@
+﻿using Mina.Extension;
+using OpenType.Extension;
+using System.IO;
+
+namespace OpenType.Tables;
+
+public class PaintColrLayersFormat9 : IPaintColrLayersFormat
+{
+    public required byte Format { get; init; }
+    public required int ColorLineOffset { get; init; }
+    public required short CenterX { get; init; }
+    public required short CenterY { get; init; }
+    public required ushort StartAngle { get; init; }
+    public required ushort EndAngle { get; init; }
+    public required uint VarIndexBase { get; init; }
+
+    public static PaintColrLayersFormat9 ReadFrom(Stream stream) => new()
+    {
+        Format = 9,
+        ColorLineOffset = stream.Read3BytesByBigEndian(),
+        CenterX = stream.ReadShortByBigEndian(),
+        CenterY = stream.ReadShortByBigEndian(),
+        StartAngle = stream.ReadUShortByBigEndian(),
+        EndAngle = stream.ReadUShortByBigEndian(),
+        VarIndexBase = stream.ReadUIntByBigEndian(),
+    };
+
+    public void WriteTo(Stream stream)
+    {
+        stream.WriteByte(Format);
+        stream.Write3BytesByBigEndian(ColorLineOffset);
+        stream.WriteShortByBigEndian(CenterX);
+        stream.WriteShortByBigEndian(CenterY);
+        stream.WriteUShortByBigEndian(StartAngle);
+        stream.WriteUShortByBigEndian(EndAngle);
+        stream.WriteUIntByBigEndian(VarIndexBase);
+    }
+
+    public int SizeOf() => Format.SizeOf() + /* ColorLineOffset sizeof(Offset24) */3 +
+        CenterX.SizeOf() + CenterY.SizeOf() +
+        StartAngle.SizeOf() + EndAngle.SizeOf() +
+        VarIndexBase.SizeOf();
+}
