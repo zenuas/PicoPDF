0676f930f111af114c47f52a1dc965dcf7b1fb85
zenuas@gmail.com <zenuas@gmail.com>
2026-03-07 13:26:38 +0900

    LayerListのPaintテーブル読み込み対応

--- font/Tables/Colr/LayerListRecord.cs
+++ font/Tables/Colr/LayerListRecord.cs
@@ -8,23 +8,35 @@ public class LayerListRecord : IExportable
 {
     public required uint NumberLayers { get; init; }
     public required uint[] PaintOffsets { get; init; }
+    public required IPaintFormat[] Paints { get; init; }
 
     public static LayerListRecord ReadFrom(Stream stream)
     {
+        var position = stream.Position;
+
         var numLayers = stream.ReadUIntByBigEndian();
+        var paintOffsets = Lists.Repeat(stream.ReadUIntByBigEndian).Take((int)numLayers).ToArray();
 
         return new()
         {
             NumberLayers = numLayers,
-            PaintOffsets = [.. Lists.Repeat(stream.ReadUIntByBigEndian).Take((int)numLayers)],
+            PaintOffsets = paintOffsets,
+            Paints = [.. paintOffsets.Select(x => PaintFormat.ReadFrom(stream.SeekTo(position + x)))],
         };
     }
 
     public void WriteTo(Stream stream)
     {
-        stream.WriteUIntByBigEndian((uint)PaintOffsets.Length);
-        PaintOffsets.Each(stream.WriteUIntByBigEndian);
+        stream.WriteUIntByBigEndian((uint)Paints.Length);
+
+        using var mem = new MemoryStream();
+        foreach (var paint in Paints)
+        {
+            stream.WriteUIntByBigEndian((uint)(SizeOf() + mem.Length));
+            paint.WriteTo(mem);
+        }
+        stream.Write(mem.ToArray());
     }
 
-    public int SizeOf() => NumberLayers.SizeOf() + (sizeof(uint) * PaintOffsets.Length);
+    public int SizeOf() => NumberLayers.SizeOf() + (sizeof(uint) * Paints.Length);
 }
