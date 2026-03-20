92f5f68646af16678a3d0241c40c726d74aacba0
zenuas@gmail.com <zenuas@gmail.com>
2026-03-21 00:40:35 +0900

    cmap抽出について説明追加

--- font/FontExtractOption.cs
+++ font/FontExtractOption.cs
@@ -13,6 +13,11 @@ public class FontExtractOption
     ];
     public (Platforms PlatformID, Encodings EncodingID, CMapFormats CMapFormat)[] OutputCMap { get; init; } = [
         new(Platforms.Unicode, Encodings.Unicode2_0_FullRepertoire, CMapFormats.Format12),
+
+        // Fonts that support only Unicode BMP characters (U+0000 to U+FFFF) on the Windows platform must use encoding 1 with a format 4 subtable.
+        // This encoding must not be used to support Unicode supplementary-plane characters.
+        // Fonts that support Unicode supplementary-plane characters (U+10000 to U+10FFFF) on the Windows platform must use encoding 10 with a format 12 subtable.
+        //  https://learn.microsoft.com/ja-jp/typography/opentype/spec/cmap#windows-platform-platform-id--3
         new(Platforms.Windows, Encodings.Windows_UnicodeBMP, CMapFormats.Format4),
         new(Platforms.Windows, Encodings.Windows_UnicodeFullRepertoire, CMapFormats.Format12),
     ];
