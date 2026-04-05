4c7c03b5649f32ef770157bb7218d6df70f93e9d
zenuas@gmail.com <zenuas@gmail.com>
2026-04-05 15:57:20 +0900

    フォント名をオプションで指定するよう変更

--- test-all/FontExport.cs
+++ test-all/FontExport.cs
@@ -7,28 +7,21 @@ namespace PicoPDF.TestAll;
 
 public class FontExport : FontRegisterCommand
 {
+    [CommandOption("font")]
+    public string Font { get; init; } = "Meiryo Bold";
+
     [CommandOption("export-chars")]
     public string ExportChars { get; init; } = "a";
 
     [CommandOption("output"), CommandOption('o')]
-    public string Output { get; init; } = "";
+    public required string Output { get; init; }
 
     public override void Run(string[] args)
     {
         var fontreg = CreateFontRegister();
-
-        foreach (var arg in args)
-        {
-            var font = fontreg.LoadComplete(arg);
-            Export(font, Output, CreateOption(ExportChars));
-        }
-
-    }
-
-    public static void Export(IOpenTypeFont font, string output, FontExtractOption extract_opt)
-    {
-        using var stream = new FileStream(output, FileMode.Create, FileAccess.ReadWrite, FileShare.Read);
-        var fontextract = FontExtract.Extract(font, extract_opt);
+        var font = fontreg.LoadComplete(Font);
+        var fontextract = FontExtract.Extract(font, CreateOption(ExportChars));
+        using var stream = new FileStream(Output, FileMode.Create, FileAccess.ReadWrite, FileShare.Read);
         FontExporter.Export(fontextract, stream);
     }
 
