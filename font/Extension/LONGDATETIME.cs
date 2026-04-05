8d3be258bbb8d26de768840f7dbfebb5fe235f6d
zenuas@gmail.com <zenuas@gmail.com>
2026-04-05 15:21:58 +0900

    LONGDATETIMEに異常値が入っていた場合の対処

--- font/Extension/LONGDATETIME.cs
+++ font/Extension/LONGDATETIME.cs
@@ -6,11 +6,21 @@ public readonly struct LONGDATETIME
 {
     public required long Value { get; init; }
 
-    public DateTime ToDateTime() => BaseTime.AddSeconds(Value);
+    public DateTime? ToDateTime()
+    {
+        try
+        {
+            return BaseTime.AddSeconds(Value);
+        }
+        catch
+        {
+            return null;
+        }
+    }
 
     public static readonly DateTime BaseTime = new(1904, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
     public static implicit operator LONGDATETIME(DateTime v) => new() { Value = (long)((v.ToUniversalTime() - BaseTime).TotalSeconds) };
     public static implicit operator LONGDATETIME(long v) => new() { Value = v };
-    public static implicit operator DateTime(LONGDATETIME v) => v.ToDateTime();
+    public static implicit operator DateTime?(LONGDATETIME v) => v.ToDateTime();
 }
