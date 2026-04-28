a97ea8ecee9f5e507417d4c38849e7b6004ef619
zenuas@gmail.com <zenuas@gmail.com>
2026-04-29 00:01:22 +0900

    GIDが最大値を超える場合、最後のMetricsを使用するよう戻す 51677e33ff762490ad50fc1f7a35b3cba4eeb76c の変更は仕様を失念したものであったためコメント追加

--- font/Utility.cs
+++ font/Utility.cs
@@ -1,5 +1,6 @@
 ﻿using Mina.Extension;
 using OpenType.Tables;
+using System;
 using System.Linq;
 
 namespace OpenType;
@@ -20,5 +21,7 @@ public static class Utility
         return font.FontHeader.UnitsPerEm == 1000 ? width : width * 1000 / font.FontHeader.UnitsPerEm;
     }
 
-    public static int GetAdvanceWidth(this IOpenTypeFont font, uint gid) => font.HorizontalMetrics.Metrics[gid].AdvanceWidth;
+    // If numberOfHMetrics is less than the total number of glyphs,
+    // then the hMetrics array is followed by an array for the left side bearing values of the remaining glyphs.
+    public static int GetAdvanceWidth(this IOpenTypeFont font, uint gid) => font.HorizontalMetrics.Metrics[Math.Min(gid, font.HorizontalHeader.NumberOfHMetrics - 1)].AdvanceWidth;
 }
