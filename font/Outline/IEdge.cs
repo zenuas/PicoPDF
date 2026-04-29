4c1746cdf3fe3b6e71b059bf058beaa6dfa24fb6
zenuas@gmail.com <zenuas@gmail.com>
2026-04-29 20:00:25 +0900

    不要になった最小・最大の実装を削除

--- font/Outline/IEdge.cs
+++ font/Outline/IEdge.cs
@@ -6,9 +6,4 @@ public interface IEdge
 {
     public Vector2 Start { get; init; }
     public Vector2 End { get; init; }
-
-    public float MinX();
-    public float MinY();
-    public float MaxX();
-    public float MaxY();
 }
--- font/Outline/Line.cs
+++ font/Outline/Line.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Numerics;
+﻿using System.Numerics;
 
 namespace OpenType.Outline;
 
@@ -7,11 +6,6 @@ public class Line : IEdge
 {
     public required Vector2 Start { get; init; }
     public required Vector2 End { get; init; }
-
-    public float MinX() => Math.Min(Start.X, End.X);
-    public float MinY() => Math.Min(Start.Y, End.Y);
-    public float MaxX() => Math.Max(Start.X, End.X);
-    public float MaxY() => Math.Max(Start.Y, End.Y);
 
     public override string ToString() => $"({Start.X}, {Start.Y}) -> ({End.X}, {End.Y})";
 }
