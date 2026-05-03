7b1a76dac0bce34fcd940bc703cacd6097c482bb
zenuas@gmail.com <zenuas@gmail.com>
2026-05-03 11:17:16 +0900

    F2DOT14を符号付に修正

--- font/Extension/F2DOT14.cs
+++ font/Extension/F2DOT14.cs
@@ -2,10 +2,10 @@
 
 public readonly struct F2DOT14
 {
-    public required ushort Value { get; init; }
+    public required short Value { get; init; }
     public float FloatValue => Value / 16384F;
 
-    public static implicit operator F2DOT14(float v) => new() { Value = (ushort)(v * 16384F) };
-    public static implicit operator F2DOT14(ushort v) => new() { Value = v };
+    public static implicit operator F2DOT14(float v) => new() { Value = (short)(v * 16384F) };
+    public static implicit operator F2DOT14(short v) => new() { Value = v };
     public static implicit operator float(F2DOT14 v) => v.FloatValue;
 }
