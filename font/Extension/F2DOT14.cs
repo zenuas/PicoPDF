dc4e1056e3b961a51285dac115d2ae05b66c72c3
zenuas@gmail.com <zenuas@gmail.com>
2026-05-03 20:14:39 +0900

    デバッグ用にToStringを実装

--- font/Extension/F2DOT14.cs
+++ font/Extension/F2DOT14.cs
@@ -8,4 +8,6 @@ public readonly struct F2DOT14
     public static implicit operator F2DOT14(float v) => new() { Value = (short)(v * 16384F) };
     public static implicit operator F2DOT14(short v) => new() { Value = v };
     public static implicit operator float(F2DOT14 v) => v.FloatValue;
+
+    public override string ToString() => FloatValue.ToString();
 }
--- font/Extension/Fixed.cs
+++ font/Extension/Fixed.cs
@@ -8,4 +8,6 @@ public struct Fixed
     public static implicit operator Fixed(float v) => new() { Value = (int)(v * 65536F) };
     public static implicit operator Fixed(int v) => new() { Value = v };
     public static implicit operator float(Fixed v) => v.FloatValue;
+
+    public override string ToString() => FloatValue.ToString();
 }
