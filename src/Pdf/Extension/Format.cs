a032b5f79e54b08dd292e77a48ae435a2588856b
zenuas@gmail.com <zenuas@gmail.com>
2026-05-28 11:03:57 +0900

    拡張メソッドの引数名統一

--- src/Pdf/Extension/Format.cs
+++ src/Pdf/Extension/Format.cs
@@ -12,20 +12,20 @@ public static class Format
 {
     public static readonly char[][] EscapeChars = [['('], [')'], ['\\']];
 
-    public static string ToEscapeString(this string s, Encoding encoding) => s.All(char.IsAscii) ? $"({s.ReplaceBeforeInsert(EscapeChars, ['\\']).ToStringByChars()})" : s.ToHexString(encoding);
+    public static string ToEscapeString(this string self, Encoding encoding) => self.All(char.IsAscii) ? $"({self.ReplaceBeforeInsert(EscapeChars, ['\\']).ToStringByChars()})" : self.ToHexString(encoding);
 
-    public static string ToHexString(this string s, Encoding encoding) => $"<{Convert.ToHexStringLower(encoding.GetBytes(s))}>";
+    public static string ToHexString(this string self, Encoding encoding) => $"<{Convert.ToHexStringLower(encoding.GetBytes(self))}>";
 
-    public static DeviceRGB ToDeviceRGB(this Color color) => new((double)color.R / 255, (double)color.G / 255, (double)color.B / 255);
+    public static DeviceRGB ToDeviceRGB(this Color self) => new((double)self.R / 255, (double)self.G / 255, (double)self.B / 255);
 
-    public static string ToPointString(this (IPoint X, IPoint Y) point, int height, string format) => ToPointString(point, (double)height, format);
-    public static string ToPointString(this (IPoint X, IPoint Y) point, double height, string format) => $"{point.X.ToPointString(format)} {(height - point.Y.ToPoint()).ToPointString(format)}";
+    public static string ToPointString(this (IPoint X, IPoint Y) self, int height, string format) => ToPointString(self, (double)height, format);
+    public static string ToPointString(this (IPoint X, IPoint Y) self, double height, string format) => $"{self.X.ToPointString(format)} {(height - self.Y.ToPoint()).ToPointString(format)}";
 
-    public static string ToPointString(this IPoint point, string format) => ToPointString(point.ToPoint(), format);
-    public static string ToPointString(this float point, string format) => ToPointString((double)point, format);
-    public static string ToPointString(this double point, string format) =>
-        format == "F%" ? point.ToString("F7", CultureInfo.InvariantCulture).TrimEnd('0').TrimEnd('.') :
-            point <= long.MaxValue &&
-            point >= long.MinValue &&
-            point % 1d == 0d ? ((long)point).ToString() : point.ToString(format, CultureInfo.InvariantCulture);
+    public static string ToPointString(this IPoint self, string format) => ToPointString(self.ToPoint(), format);
+    public static string ToPointString(this float self, string format) => ToPointString((double)self, format);
+    public static string ToPointString(this double self, string format) =>
+        format == "F%" ? self.ToString("F7", CultureInfo.InvariantCulture).TrimEnd('0').TrimEnd('.') :
+            self <= long.MaxValue &&
+            self >= long.MinValue &&
+            self % 1d == 0d ? ((long)self).ToString() : self.ToString(format, CultureInfo.InvariantCulture);
 }
