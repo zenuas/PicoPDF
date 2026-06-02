e9ed201536d024a175cabb2561f53373cbf49724
zenuas@gmail.com <zenuas@gmail.com>
2026-06-02 12:42:43 +0900

    パスワード切り詰めを関数化

--- src/Pdf/Documents/Security/Encryption.cs
+++ src/Pdf/Documents/Security/Encryption.cs
@@ -6,17 +6,19 @@ namespace PicoPDF.Pdf.Documents.Security;
 
 public static class Encryption
 {
-    public static readonly byte[] PasswordPadding_Revision4 = [
+    public static readonly byte[] PasswordPaddingBytes = [
         0x28, 0xBF, 0x4E, 0x5E, 0x4E, 0x75, 0x8A, 0x41, 0x64, 0x00, 0x4E, 0x56, 0xFF, 0xFA, 0x01, 0x08,
         0x2E, 0x2E, 0x00, 0xB6, 0xD0, 0x68, 0x3E, 0x80, 0x2F, 0x0C, 0xA9, 0xFE, 0x64, 0x53, 0x69, 0x7A,
     ];
 
+    public static byte[] PadOrTruncatePassword32Bytes(byte[] password) => [.. password.Concat(PasswordPaddingBytes).Take(32)];
+
     public static byte[] ComputeOwnerPassword_Revision3(byte[] user_password, byte[] owner_password, int size)
     {
-        var hash = MD5.HashData([.. owner_password.Concat(PasswordPadding_Revision4).Take(32)]);
+        var hash = MD5.HashData(PadOrTruncatePassword32Bytes(owner_password));
         for (var i = 0; i < 50; i++) hash = MD5.HashData(hash[0..size]);
 
-        var owner_key = user_password.Concat(PasswordPadding_Revision4).Take(32).ToArray();
+        var owner_key = PadOrTruncatePassword32Bytes(user_password);
         Span<byte> key = stackalloc byte[size];
         for (var i = 0; i < 20; i++)
         {
@@ -24,10 +26,5 @@ public static class Encryption
             _ = Arcfour.Encrypt(Arcfour.InitializeKey(key), owner_key);
         }
         return owner_key;
-    }
-
-    public static byte[] CreatePassword_Revision6()
-    {
-        return [];
     }
 }
