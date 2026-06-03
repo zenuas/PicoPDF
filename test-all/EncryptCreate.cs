46a10bf47d0e10e673cf7d2d6746e1ce634e2512
zenuas@gmail.com <zenuas@gmail.com>
2026-06-04 01:10:25 +0900

    暗号化実装のためstring値を設定

--- test-all/EncryptCreate.cs
+++ test-all/EncryptCreate.cs
@@ -13,17 +13,20 @@ public class EncryptCreate : FontRegisterCommand
         var document1 = new Document() { Version = 17, FontRegister = fontreg };
         var (width, height) = PageSize.GetPageSize(PageSizes.A4, Orientations.Horizontal);
         _ = document1.NewPage(width, height);
+        document1.AddInfo("Title");
         document1.AddEncrypt(CFM.None, "xyz987", "abc123", UserAccessPermissions.Default);
         document1.Save("test-case/encrypt-none-create.pdf");
 
         var document2 = new Document() { Version = 17, FontRegister = fontreg };
         _ = document2.NewPage(width, height);
         document2.AddEncrypt(CFM.AESV2, "xyz987", "abc123", UserAccessPermissions.Default);
+        document2.AddInfo("Title");
         document2.Save("test-case/encrypt-aesv2-create.pdf");
 
         var document3 = new Document() { Version = 20, FontRegister = fontreg };
         _ = document3.NewPage(width, height);
         document3.AddEncrypt(CFM.AESV3, "xyz987", "abc123", UserAccessPermissions.Default);
+        document3.AddInfo("Title");
         document3.Save("test-case/encrypt-aesv3-create.pdf");
     }
 }
