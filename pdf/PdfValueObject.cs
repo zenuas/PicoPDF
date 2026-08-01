490f0278b117303b14445b86b0607447921c060d
zenuas@gmail.com <zenuas@gmail.com>
2026-08-02 02:15:11 +0900

    間接オブジェクトとして辞書以外も参照できるよう追加

---
+++ pdf/PdfValueObject.cs
@@ -0,0 +1,34 @@
+﻿using Mina.Extension;
+using Pdf.Documents;
+using Pdf.Elements;
+using System.Buffers;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Pdf;
+
+public class PdfValueObject<T> : IPdfObject where T : ElementValue
+{
+    public int IndirectIndex { get; set; }
+    public required T Element { get; init; }
+
+    public virtual void BeforeExport(PdfExportOption option)
+    {
+    }
+
+    public virtual IEnumerable<IHaveReferences> GetReferences()
+    {
+        if (Element is IHaveReferences v)
+        {
+            yield return v;
+        }
+    }
+
+    public void Export(Document document, Stream stream, PdfExportOption option)
+    {
+        stream.Write($"{IndirectIndex} 0 obj\n");
+        using var converter = document.Encrypt?.StringHandler?.CreateEncrypterConverter(IndirectIndex, 0);
+        stream.Write(Element.ToElementString(converter));
+        stream.Write("endobj\n\n");
+    }
+}
