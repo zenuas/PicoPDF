934c24e42ecf3db2b9749cbdd618272134bd7b41
zenuas@gmail.com <zenuas@gmail.com>
2026-09-12 09:19:34 +0900

    ScriptRecordを分離

--- font/Tables/Common/ScriptListTable.cs
+++ font/Tables/Common/ScriptListTable.cs
@@ -9,21 +9,23 @@ namespace OpenType.Tables.Common;
 public class ScriptListTable
 {
     public required ushort ScriptCount { get; init; }
-    public required (string ScriptTag, Offset16 ScriptOffset, ScriptTable ScriptTable)[] ScriptRecords { get; init; }
+    public required ScriptRecord[] ScriptRecords { get; init; }
 
     public static ScriptListTable ReadFrom(Stream stream)
     {
         var position = stream.Position;
 
         var script_count = stream.ReadUShortByBigEndian();
+
+        var script_records_offset = stream.Position;
         var script_records = Lists.Repeat(() => (ScriptTag: Encoding.ASCII.GetString(stream.ReadExactly(4)), ScriptOffset: stream.ReadOffset16())).Take(script_count).ToArray();
 
         return new()
         {
             ScriptCount = script_count,
-            ScriptRecords = [.. script_records.Select(x => (x.ScriptTag, x.ScriptOffset, ScriptTable.ReadFrom(stream.SeekTo(position + x.ScriptOffset.Value))))],
+            ScriptRecords = [.. Lists.Sequence(script_records_offset, ScriptRecord.SizeOf()).Select(x => ScriptRecord.ReadFrom(stream.SeekTo(x), position)).Take(script_count)],
         };
     }
 
-    public int SizeOf() => ScriptCount.SizeOf() + ((/* sizeof(ScriptTag) */4 + Offset16.SizeOf()) * ScriptRecords.Length);
+    public int SizeOf() => ScriptCount.SizeOf() + (ScriptRecord.SizeOf() * ScriptRecords.Length);
 }
---
+++ font/Tables/Common/ScriptRecord.cs
@@ -0,0 +1,29 @@
+﻿using Mina.Extension;
+using OpenType.Extension;
+using System.IO;
+using System.Text;
+
+namespace OpenType.Tables.Common;
+
+public class ScriptRecord
+{
+    public required string ScriptTag { get; init; }
+    public required Offset16 ScriptOffset { get; init; }
+    public required ScriptTable ScriptTable { get; init; }
+
+    public static ScriptRecord ReadFrom(Stream stream, long script_list_offset)
+    {
+        var script_tag = Encoding.ASCII.GetString(stream.ReadExactly(4));
+        var script_offset = stream.ReadOffset16();
+
+        return new()
+        {
+            ScriptTag = script_tag,
+            ScriptOffset = script_offset,
+            // Offset to Script table, from beginning of ScriptList
+            ScriptTable = ScriptTable.ReadFrom(stream.SeekTo(script_list_offset + script_offset.Value)),
+        };
+    }
+
+    public static int SizeOf() => /* sizeof(ScriptTag) */4 + /* sizeof(ScriptOffset) */Offset16.SizeOf();
+}
